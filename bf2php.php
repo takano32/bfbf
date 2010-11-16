e972fabc28048873487c417aeb6181dc6959bd29
TAKANO Mitsuhiro <takano32@gmail.com>
2010-11-16 15:50:48 +0900

    impl bf2php.php

--- bf2php.php
+++ bf2php.php
@@ -1,12 +1,43 @@
 #!/usr/bin/env php
 <?php
+echo '<?php' . PHP_EOL;
+echo '$dseg = array();' . PHP_EOL;
+echo '$dseg[0] = 0;' . PHP_EOL;
+echo '$pc = 0;' . PHP_EOL;
+echo '$dx = 0;' . PHP_EOL;
+
+
+
+
 $stdin = file_get_contents("php://stdin");
 
 for ($i = 0; $i < strlen($stdin); $i++) {
-	echo $ch = $stdin[$i];
+	$ch = $stdin[$i];
 	switch ($ch) {
-	case 'h':
-		echo 'this is h';
+	case '>':
+		echo '$dx++;' . PHP_EOL;
+		echo 'if (isset($dseg[$dx]) == FALSE) $dseg[$dx] = 0;' . PHP_EOL;
+		break;
+	case '<':
+		echo '$dx--;' . PHP_EOL;
+		break;
+	case '+':
+		echo '$dseg[$dx]++;' . PHP_EOL;
+		break;
+	case '-':
+		echo '$dseg[$dx]--;' . PHP_EOL;
+		break;
+	case '.':
+		echo 'print chr($dseg[$dx]);' . PHP_EOL;
+		break;
+	case ',':
+		// input from user
+		break;
+	case '[':
+		echo 'while ($dseg[$dx] != 0) {' . PHP_EOL;
+		break;
+	case ']':
+		echo '}' . PHP_EOL;
 		break;
 	default:
 	}

37c0aab355785abd293f0ea965b2abf4963a0c46
esehara <esehara@gmail.com>
2013-10-11 01:12:13 +0900

    bf2clj: incomplete, but output hello, world ;)

--- bf2clj.clj
+++ bf2clj.clj
@@ -1,11 +1,62 @@
-#!/usr/bin/env clj
+;; #!/usr/bin/env clj
 ;; convert from Clojure to Brainfuck
+(defn stdin []
+  (line-seq
+  (java.io.BufferedReader. *in* )))
 
-(def input (read))
-(println input)
-(println input)
-(println input)
-(println input)
-(println input)
+(defn clojurefuck
+  [char]
+  (println 1))
+
+(def pointer 0)
+
+(defn init-print []
+  (println "
+
+(def pointer 0)
+
+(defn memoryname [] (str \"m\" pointer))
+
+(defn get-value-from-memory []
+  (eval (symbol (memoryname))))
+
+(defn inc-pointer
+  [] (def pointer (+ pointer 1)))
+
+(defn dec-pointer
+  [] (def pointer (- pointer 1)))
+
+(defn set-memory? []
+  (boolean (resolve (symbol (memoryname)))))
+
+(defn set-or-zero []
+    (if (set-memory?) (get-value-from-memory) 0))
 
 
+(defn set-pointer [set-value]
+  (intern *ns* (symbol (memoryname)) set-value))
+
+(defn inc-memory [] (set-pointer (+ (set-or-zero) 1)))
+(defn dec-memory [] (set-pointer (- (set-or-zero) 1)))
+
+"))
+
+(defn clojurefuck
+  [token]
+  (do
+      (cond
+   (= token \[) (println "
+((fn []
+     (loop []
+       (if (> (set-or-zero) 0) (do ")
+   (= token \]) (println "(recur)) nil))))")
+   (= token \+) (println "(inc-memory)")
+   (= token \-) (println "(dec-memory)")
+   (= token \>) (println "(inc-pointer)")
+   (= token \<) (println "(dec-pointer)")
+   (= token \.) (println "(print (char (set-or-zero)))")))
+  )
+
+(init-print)
+(doall
+ (map clojurefuck (str (stdin))))
